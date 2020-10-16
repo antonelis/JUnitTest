e22140a389946be1b83826e42b1858517a74d336
Anton Johansson <anton.elis@hotmail.com>
2020-10-16 10:58:13 +0200

    actoring

--- src/test/java/yatzy/YatzyTest.java
+++ src/test/java/yatzy/YatzyTest.java
@@ -8,65 +8,65 @@ public class YatzyTest {
     @Test
     public void chance_scores_sum_of_all_dice() {
         int expected = 15;
-        int actual = Yatzy.chance(2,3,4,5,1);
+        int actual = Yatzy.chance(new Yatzy.DiceHand(2, 3, 4, 5, 1));
         assertEquals(expected, actual);
-        assertEquals(16, Yatzy.chance(3,3,4,5,1));
+        assertEquals(16, Yatzy.chance(new Yatzy.DiceHand(3, 3, 4, 5, 1)));
     }
 
     @Test public void yatzy_scores_50() {
         int expected = 50;
-        int actual = Yatzy.yatzy(4,4,4,4,4);
+        int actual = Yatzy.yatzy(new Yatzy.DiceHand(4, 4, 4, 4, 4));
         assertEquals(expected, actual);
-        assertEquals(50, Yatzy.yatzy(6,6,6,6,6));
-        assertEquals(0, Yatzy.yatzy(6,6,6,6,3));
+        assertEquals(50, Yatzy.yatzy(new Yatzy.DiceHand(6, 6, 6, 6, 6)));
+        assertEquals(0, Yatzy.yatzy(new Yatzy.DiceHand(6, 6, 6, 6, 3)));
     }
 
     @Test public void test_1s() {
-        assertEquals(Yatzy.ones(1, 2, 3, 4, 5), 1);
-        assertEquals(2, Yatzy.ones(1,2,1,4,5));
-        assertEquals(0, Yatzy.ones(6,2,2,4,5));
-        assertEquals(4, Yatzy.ones(1,2,1,1,1));
+        assertEquals(Yatzy.ones(new Yatzy.DiceHand(1, 2, 3, 4, 5)), 1);
+        assertEquals(2, Yatzy.ones(new Yatzy.DiceHand(1, 2, 1, 4, 5)));
+        assertEquals(0, Yatzy.ones(new Yatzy.DiceHand(6, 2, 2, 4, 5)));
+        assertEquals(4, Yatzy.ones(new Yatzy.DiceHand(1, 2, 1, 1, 1)));
     }
 
     @Test
     public void test_2s() {
-        assertEquals(4, Yatzy.twos(1,2,3,2,6));
-        assertEquals(10, Yatzy.twos(2,2,2,2,2));
+        assertEquals(4, Yatzy.twos(new Yatzy.DiceHand(1, 2, 3, 2, 6)));
+        assertEquals(10, Yatzy.twos(new Yatzy.DiceHand(2, 2, 2, 2, 2)));
     }
 
     @Test
     public void test_threes() {
-        assertEquals(6, Yatzy.threes(1,2,3,2,3));
-        assertEquals(12, Yatzy.threes(2,3,3,3,3));
+        assertEquals(6, Yatzy.threes(new Yatzy.DiceHand(1, 2, 3, 2, 3)));
+        assertEquals(12, Yatzy.threes(new Yatzy.DiceHand(2, 3, 3, 3, 3)));
     }
 
     @Test
     public void fours_test()
     {
-        assertEquals(12, new Yatzy(4,4,4,5,5).fours());
-        assertEquals(8, new Yatzy(4,4,5,5,5).fours());
-        assertEquals(4, new Yatzy(4,5,5,5,5).fours());
+        assertEquals(12, Yatzy.fours(new Yatzy.DiceHand(4,4,4,3,3)));
+        assertEquals(8, Yatzy.fours(new Yatzy.DiceHand(4,4,3,3,3)));
+        assertEquals(4, Yatzy.fours(new Yatzy.DiceHand(4,2,1,3,5)));
     }
 
     @Test
     public void fives() {
-        assertEquals(10, new Yatzy(4,4,4,5,5).fives());
-        assertEquals(15, new Yatzy(4,4,5,5,5).fives());
-        assertEquals(20, new Yatzy(4,5,5,5,5).fives());
+        assertEquals(10, Yatzy.fives(new Yatzy.DiceHand(5,5,4,3,2)));
+        assertEquals(15, Yatzy.fives(new Yatzy.DiceHand(5,5,5,3,2)));
+        assertEquals(20, Yatzy.fives(new Yatzy.DiceHand(5,5,5,5,2)));
     }
 
     @Test
     public void sixes_test() {
-        assertEquals(0, new Yatzy(4,4,4,5,5).sixes());
-        assertEquals(6, new Yatzy(4,4,6,5,5).sixes());
-        assertEquals(18, new Yatzy(6,5,6,6,5).sixes());
+        assertEquals(0, Yatzy.sixes(new Yatzy.DiceHand(1,2,3,4,5)));
+        assertEquals(6, Yatzy.sixes(new Yatzy.DiceHand(6,5,4,5,2)));
+        assertEquals(18, Yatzy.sixes(new Yatzy.DiceHand(6,6,6,5,2)));
     }
 
     @Test
     public void one_pair() {
-        assertEquals(6, Yatzy.score_pair(3,4,3,5,6));
-        assertEquals(10, Yatzy.score_pair(5,3,3,3,5));
-        assertEquals(12, Yatzy.score_pair(5,3,6,6,5));
+        assertEquals(6, Yatzy.score_pair(3, 4, 3, 5, 6));
+        assertEquals(10, Yatzy.score_pair(5, 3, 3, 3, 5));
+        assertEquals(12, Yatzy.score_pair(5, 3, 6, 6, 5));
     }
 
     @Test
