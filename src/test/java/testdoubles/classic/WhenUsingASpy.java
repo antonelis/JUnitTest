85f56552fced439eccb77c542a49bb8ef952c8be
Anton Johansson <anton.elis@hotmail.com>
2020-10-19 08:36:19 +0200

    Test WhenUsingASpy classic java implementation

---
+++ src/test/java/testdoubles/classic/WhenUsingASpy.java
@@ -0,0 +1,31 @@
+package testdoubles.classic;
+
+import org.junit.jupiter.api.Test;
+import testdoubles.Authorizer;
+import testdoubles.System;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class WhenUsingASpy {
+
+    @Test
+    public void we_want_to_ensure_that_the_spy_was_called() {
+        AcceptingAuthorizerSpy spy = new AcceptingAuthorizerSpy();
+        System system = new System(spy);
+
+        system.login("bob", "SecretPassword");
+
+        assertThat(spy.authoriseWasCalled).isTrue();
+    }
+
+    static class AcceptingAuthorizerSpy implements Authorizer {
+        public boolean authoriseWasCalled = false;
+
+        @Override
+        public Boolean authorise(String username, String password) {
+            authoriseWasCalled = true;
+            return true;
+        }
+    }
+}
